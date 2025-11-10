5c873ac6aa037688c8349b1a8c8bc651ec2fc8ad
Gema Gonzalez <gemaaa1911@gmail.com>
2025-11-10 14:09:49 +0100

    He añadido los pasajeros al array

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //Completar Codigo
+        //Creamos el array de pasajeros
+        List<Pasajero> pasajeros = new ArrayList<>();
+
+        //Creamos las 5 balsas
         Balsa balAcasta = new Balsa(1, 0.5);
         Balsa balBanff = new Balsa(2, 1);
         Balsa balCadiz = new Balsa(3, 2);
         Balsa balDeimos = new Balsa(4, 4);
         Balsa balExpedición	= new Balsa(5, 8);
+
+        //Hacemos un bucle para crear todos los pasajeros y los añadimos al arrayList
+
+        for (int id=1;id<=352;id++){
+            int prioridad = (int)(Math.random()*(5-1)+1);
+            if(prioridad == 5){
+                prioridad = 1;
+            }
+            Pasajero pasajero = new Pasajero(id, prioridad);
+            pasajeros.add(pasajero);
+            System.out.println(pasajero);
+        }
+
+
+
     }
 }
