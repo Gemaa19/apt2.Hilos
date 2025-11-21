98c7f01fe87b02678e91ae44949877303573e642
Gema Gonzalez <gemaaa1911@gmail.com>
2025-11-21 09:09:47 +0100

    he añadido a rescate un try catch para que espere el tiempo de cada balsa y he añadido al constructor un string para poder poner el nombre de la balsa

--- src/Rescate.java
+++ src/Rescate.java
@@ -1,32 +1,42 @@
 import java.util.List;
 
-public class Rescate extends Thread {
+public class Rescate implements Runnable {
     private boolean hayGente;
     private Barco barco;
     private Balsa balsa;
-    private int capacidad;
+    //private int capacidad;
 
-    public Rescate(Barco barco, Balsa balsa, List<Pasajero> pasajeros, int capacidad){
+    public Rescate(Barco barco, Balsa balsa, String nombreBalsa){
         this.barco = barco;
         this.balsa = balsa;
-        this.capacidad = capacidad;
     }
 
     @Override
     public void run() {
         while(hayGente){
-            barco.obtenerPasajPriori();
-            System.out.println("Balsa coge gente");
-            balsa.getCapacidad();
+            for (int i = 0; i < balsa.getCapacidad(); i++) {
+                Pasajero p = barco.obtenerPasajPriori();
+                System.out.println("Balsa coge gente");
+                balsa.recogerPasajero(p);
+            }
+            System.out.println(balsa);
+            try{
+                Thread.sleep((long)balsa.getTiempo()*1000);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+            if(!barco.hayPasajeros()){
+                hayGente = false;
+            }
         }
     }
 
     @Override
     public String toString() {
         return "Rescate{" +
+                "hayGente=" + hayGente +
                 ", barco=" + barco +
                 ", balsa=" + balsa +
-                ", capacidad=" + capacidad +
                 '}';
     }
 }
